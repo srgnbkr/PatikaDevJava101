3778999f323bc8b98f299225dc74dc4e462faac8
srgnbkr <53319517+srgnbkr@users.noreply.github.com>
2021-07-09 12:48:04 +0300

    ödev

--- ForLoopDemo/src/Main.java
+++ ForLoopDemo/src/Main.java
@@ -3,18 +3,25 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		int number,total =0;
+		int number,total=0,varNumber,newTotal = 0;
 		Scanner input = new Scanner(System.in);
 		System.out.println("Sayı Giriniz: ");
 		number = input.nextInt();
 		
-		for(int i = 0; i<=number ; i++) {
+		for(int i = 1; i<=number ; i++) {
 			if(i%12==0) {
-				total = i +total;
+				System.out.println(i);
+				varNumber = number / 12;
+				total = (i +total);
+				newTotal = total / varNumber;
+				
+				
 			}
 			
 		}
-		System.out.println(total);
+		System.out.println("3'e Ve 4'e Bölünen tam sayıların toplamını :"+total);
+		System.out.println("3'e Ve 4'e Bölünen tam sayıların toplamının ortalaması :"+newTotal);
+		
 
 	}
 
